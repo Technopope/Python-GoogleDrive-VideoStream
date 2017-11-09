5572e1d81ecd95fe5d638490d349dd74816970f9
Dylan Durdle <ddurdle@gmail.com>
2017-11-09 13:40:07 -0500

    major sync

---
+++ resources/libgui/xbmcaddon.py
@@ -0,0 +1,95 @@
+'''
+    Copyright (C) 2014-2017 ddurdle
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+'''
+
+
+#
+# The purpose of this class is to override  xbmcaddon and supply equivalent subroutines when ran without KODI
+#
+
+
+class xbmcaddon:
+    # CloudService v0.3.0
+
+    ##
+    ##
+    def __init__(self):
+        return
+
+    ##
+    # return the setting from DBM
+    ##
+    def getSetting(self,key):
+        return ''
+
+    ##
+    # return the setting from DBM
+    ##
+    def getLocalizedString(self,key):
+        return ''
+
+
+    ##
+    # Get the token of name with value provided.
+    # returns: str
+    ##
+    def getToken(self,name):
+        if name in self.auth:
+            return self.auth[name]
+        else:
+            return ''
+
+    ##
+    # Get the count of authorization tokens
+    # returns: int
+    ##
+    def getTokenCount(self):
+        return len(self.auth)
+
+    ##
+    # Save the latest authorization tokens
+    ##
+    def saveTokens(self,instanceName,addon):
+        for token in self.auth:
+            addon.setSetting(instanceName + '_'+token, self.auth[token])
+
+    ##
+    # load the latest authorization tokens
+    ##
+    def loadToken(self,instanceName,addon, token):
+        try:
+            tokenValue = addon.getSetting(instanceName + '_'+token)
+            if tokenValue != '':
+              self.auth[token] = tokenValue
+              return True
+            else:
+              return False
+        except:
+            return False
+
+    ##
+    # load the latest authorization tokens
+    ##
+    def isToken(self,instanceName, addon, token):
+        try:
+            if self.auth[token] != '':
+              return True
+            else:
+              return False
+        except:
+            return False
--- test.py
+++ test.py
@@ -1,7 +1,3 @@
-from resources.lib import cloudservice
-from resources.lib import gdrive_api2
-
-
 
 import sys
 import re
@@ -9,7 +5,20 @@ import os
 
 print "hello world"
 
+
+# global variables
+import addon_parameters
+addon = addon_parameters.addon
+cloudservice3 = addon_parameters.cloudservice3
+cloudservice2 = addon_parameters.cloudservice2
+
+from resources.lib import cloudservice
+from resources.lib import gdrive_api2
+
+#from resources.lib import kodi_common
+
+
 #try:
-cloudservice = gdrive_api2.gdrive(None,None,str(sys.argv[2]),'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/532.0 (KHTML, like Gecko) Chrome/3.0.195.38 Safari/532.0',None, DBM=str(sys.argv[1]))
+cloudservice = cloudservice2(None,addon,str(sys.argv[2]),'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/532.0 (KHTML, like Gecko) Chrome/3.0.195.38 Safari/532.0',None, DBM=str(sys.argv[1]))
 #except:
 #    print "Usage: python gdServer.py DBM instancename\nexample: python gdServer.py gdrive.db username\n"
