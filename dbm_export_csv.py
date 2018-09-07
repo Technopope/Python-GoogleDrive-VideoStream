5c7b5441043675a261b581000ec8594e4758b98b
Dylan Durdle <ddurdle@gmail.com>
2018-09-07 09:19:13 -0400

    fix export CSV script

--- dbm_export_csv.py
+++ dbm_export_csv.py
@@ -8,21 +8,21 @@ import anydbm
 try:
     dbmfile = str(sys.argv[1])
     csvfile = str(sys.argv[2])
-
-    dbm = anydbm.open(dbmfile,'r')
-
-    file = open(csvfile, "c")
-    print "exporting the following key,value pairs\n"
-    for key in dbm:
-        try:
-            value = dbm[key];
-        except:
-            value = '***corrupt***'
-        print str(key) + ',' + str(value) + "\n"
-        file.write(str(key) + ',' + str(value) + "\n")
-
-
-    dbm.close()
 except:
     print "This tool exports your dbm to CSV format.  This can be used to recover data from a corrupt dbm.\nUsage:\n"
     print "python dbm_export_csv.py <dbm_file.db> <settings.csv>"
+
+dbm = anydbm.open(dbmfile,'r')
+
+file = open(csvfile, "c")
+print "exporting the following key,value pairs\n"
+for key in dbm:
+    try:
+        value = dbm[key];
+    except:
+        value = '***corrupt***'
+    print str(key) + ',' + str(value) + "\n"
+    file.write(str(key) + ',' + str(value) + "\n")
+
+file.close()
+dbm.close()
