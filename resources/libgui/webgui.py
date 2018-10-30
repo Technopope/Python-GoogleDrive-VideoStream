404fe0e2361f98cf83967ee9790d3b2f58346427
Dylan Durdle <ddurdle@gmail.com>
2018-10-30 11:36:32 -0400

    testing

--- resources/libgui/webgui.py
+++ resources/libgui/webgui.py
@@ -36,6 +36,7 @@ from resources.libgui import xbmcplugin
 from resources.libgui import settingsdbm
 from resources.libgui import xbmc
 from resources.libgui import xbmcaddon
+import numpy as np
 
 
 if constants.CONST.DEBUG:
@@ -176,8 +177,9 @@ class WebGUIServer(ThreadingMixIn,HTTPServer):
                                 id = entry[1]
                                 hash = entry[0]
                                 ids = entry[1].split("|")
-                                self.MD5List[hash] = ids
-                                print "hash = " + str(hash) + " ids = " + str(ids) + "\n"
+                                self.MD5List[hash] = np.asarray(ids)
+                                #self.MD5List[hash] = ids
+                                #print "hash = " + str(hash) + " ids = " + str(ids) + "\n"
 #                                if hash not in self.MD5List.keys():
 #                                    self.MD5List[hash] = []
 #                                    self.MD5List[hash].append(id)
